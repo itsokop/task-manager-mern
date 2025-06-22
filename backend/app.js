7d24de7315afac624e51c6ccabad3281c0e0c32f
Om Prakash Shaw <omprakash@stylopay.com>
2025-06-22 17:48:14 +0530

    vercel

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,16 @@ import 'dotenv/config';
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "https://task-managment-frontend-six.vercel.app/",
+            "http://localhost:5173",
+        ],
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 // Routes
@@ -35,17 +44,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-// Middleware
-// app.use(cors());
-app.use(
-    cors({
-        origin: [
-            "https://task-management.vercel.app",
-            "http://localhost:5173",
-        ],
-        credentials: true,
-    })
-);
 
 export default app;
